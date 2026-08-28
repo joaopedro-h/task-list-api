e02c98659a60bc7336e63400d74a0761c2c5b0f6
João Pedro <TocooXD@gmail.com>
2026-08-27 22:20:56 -0300

    feat: adiciona validações utilizando Yup

--- sources/app/controllers/UserController.js
+++ sources/app/controllers/UserController.js
@@ -1,10 +1,23 @@
 import connection from "../../database/connection";
+import * as Yup from "yup";
 import encryptPassword from "../../utils/encryptPassword";
 import decryptPassword from "../../utils/decryptPassword";
 
 class UserController {
 
     async store(req, res){
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                error: "Dados inválidos!"
+            });            
+        }
 
         const {name, email, password} = req.body;
 
@@ -33,6 +46,24 @@ class UserController {
     }
 
     async update(req, res){
+
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().min(6).when("oldPassword", (oldPassword, field) => 
+                oldPassword ? field.required() : field
+            ),
+            confirmPassword: Yup.string().when("password", (password, field) => 
+                password ? field.required().oneOf([Yup.ref("password")]) : field
+            ),
+        });
+        
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                error: "Dados inválidos!"
+            });            
+        }
 
         const {name, email, oldPassword, password} = req.body;
 
@@ -74,7 +105,6 @@ class UserController {
 
         const fields = [];
         const values = [];
-
 
         if (name) {
             fields.push("name = ?");
