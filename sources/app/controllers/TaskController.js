da574fbdabbc5dccb4ec4d44519e1d85668a794d
João Pedro <TocooXD@gmail.com>
2026-08-29 19:44:27 -0300

    feat: adiciona métodos de criar e listar tarefas do usuário

---
+++ sources/app/controllers/TaskController.js
@@ -0,0 +1,59 @@
+import connection from "../../database/connection";
+import * as Yup from "yup";
+
+class TaskController {
+
+    async index(req, res) {
+
+        const schema = Yup.object().shape({
+            check: Yup.boolean().required()
+        });
+
+        if (!(await schema.isValid(req.query))) {
+            return res.status(400).json({
+                error: "Dados inválidos!"
+            });             
+        }
+
+        const {check} = req.query;
+
+        const [userTasks] = await connection.execute(
+            `SELECT * FROM tasks
+            WHERE user_id = ? AND check_task = ?`, [req.userId,check]
+        );
+
+        return res.status(200).json({
+            message: "Tarefas com o status selecionado",
+            tarefas: userTasks
+        });
+
+    }
+
+    async store(req, res) {
+
+        const schema = Yup.object().shape({
+            task: Yup.string().required()
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                error: "Falha ao cadastrar!"
+            });              
+        }
+
+        const {task} = req.body;
+
+        const sqlCreateTask = await connection.execute(
+        `INSERT INTO tasks (task, user_id)
+        VALUES (?,?)`, [task,req.userId]);
+
+        return res.status(201).json({
+            message: "Tarefa criada com sucesso!",
+            "ID da tarefa": sqlCreateTask[0].insertId
+        });
+        
+    }
+ 
+}
+
+export default new TaskController();
--- sources/routes.js
+++ sources/routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import TaskController from "./app/controllers/TaskController";
 import AuthMiddleware from "./app/middlewares/authentication";
 
 const routes = new Router(); // "routes" armazena o Router onde as rotas serão criadas.
@@ -12,6 +13,10 @@ routes.get("/sessions", SessionController.store);
 routes.use(AuthMiddleware);
 
 routes.put("/users", UserController.update);
+
+routes.post("/tasks", TaskController.store);
+
+routes.get("/tasks", TaskController.index);
 
 
 export default routes; // Exporta o routes para ser usado no "app.js".
